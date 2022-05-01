53e40024664ce2cf3e0815314ccae37acd87721b
Steve Mena <smena@users.noreply.github.com>
2022-05-01 18:52:56 -0400

    Use setuptools instead of requirements

---
+++ cartridge-generator/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup (
+    name = 'wildcat',
+    version = '0.1',
+    py_modules = ['wildcat'],
+    install_requires = [
+        'markovify==0.9.4',
+        'argparse==1.4.0',
+        'flask==2.1.2',
+        'python-dotenv==0.20.0'
+    ],
+    entry_points = '''
+        [console_scripts]
+        module=module:__init__
+    '''
+)
