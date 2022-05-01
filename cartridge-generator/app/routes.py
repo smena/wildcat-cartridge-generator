dd52d599556eea87586e822ed2d829227de23d9e
Steve Mena <smena@users.noreply.github.com>
2022-05-01 10:25:06 -0400

    Add templates

--- cartridge-generator/app/routes.py
+++ cartridge-generator/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return generate_cartridge()
+    user = {'username': 'fooooooo'}
+    return render_template('index.html', title='Home', user=user)
